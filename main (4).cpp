c482016a43f1e47d210e8f3f301288893f6d8feb
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-06 20:59:23 +0330

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  a,b,c,d,e,sum;
+   cout<<"a:";cin >>a ;
+   cout<<"b:";cin>>b;
+   cout<<"c:";cin>>c;
+   cout<<"d:";cin>>d;
+   cout<<"e:";cin>>e;
+   sum=a+b+c+d+c;
+   cout<<"sum/5";
+   cout<<"bigger num";
+   return 0;
+}
+  
